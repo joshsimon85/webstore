969054824fdcaaaddc52b78e9918c595bf87c4ab
Josh <joshsimon85@yahoo.com>
2020-01-24 16:23:09 -0700

    Initial commit

---
+++ routes/all.js
@@ -0,0 +1,27 @@
+var express = require('express');
+var router = express.Router();
+var _ = require('underscore');
+
+module.exports = function(app) {
+  function setActiveNavTo(title) {
+    var activeItem = _(app.locals.links).findWhere({ active: true });
+
+    if (activeItem) { activeItem.active = false; }
+    _(app.locals.links).findWhere({ title: title }).active = true;
+  }
+
+  router.get('/', function(req, res, next) {
+    res.render('index', {
+      title: 'Web Store'
+    });
+  });
+
+  router.get('/about', function(req, res, next) {
+    var title = 'About';
+
+    setActiveNavTo(title);
+    res.render('about', { title: 'About' });
+  });
+
+  return router;
+};
