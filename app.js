f6693cd96e56155ce4c86b5dded4b6e1a6a88a62
Josh <joshsimon85@yahoo.com>
2020-01-27 07:15:43 -0700

    Complete express app

--- app.js
+++ app.js
@@ -3,14 +3,22 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var stylus = require('stylus');
+var nib = require ('nib');
 
 var app = express();
-app.locals = require('./locals');
 
-var routes = require('./routes/all')(app);
+var routes = require('./routes/all');
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+
+app.use(stylus.middleware({
+  src: path.join(__dirname, 'public'),
+  compile: function(str, p) {
+    return stylus(str).set('filename', p).use(nib());
+  }
+}));
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -25,6 +33,7 @@ app.use(function(req, res, next) {
   next(createError(404));
 });
 
+app.locals.basedir = path.join(__dirname, 'views');
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
